e0fdebe08a6152a1913bcc7d71bf18ec56f1bf41
Leandro Ferreira <leanfj@hotmail.com>
2024-04-02 13:22:45 -0300

    test: atualizar testes do App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,22 +1,26 @@
-import './matchMediaMock';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-import { act } from 'react-dom/test-utils';
+import "./matchMediaMock";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import { act } from "react-dom/test-utils";
 
 // Extend the type definition of JestMatchers<HTMLElement> to include toBeInTheDocument
 declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace jest {
-    interface Matchers<R, T> {
+    interface Matchers<R> {
+      // eslint-disable-next-line @typescript-eslint/method-signature-style
       toBeInTheDocument(): R;
     }
   }
 }
 
 describe("App", () => {
-  test('renders learn react link', async () => {
-    // eslint-disable-next-line testing-library/no-unnecessary-act
-    await act( async () => { render(<App/>) });
+  test("renders learn react link", async () => {
+    await act(async () => {
+      // eslint-disable-next-line react/react-in-jsx-scope
+      render(<App />);
+    });
     const linkElement = screen.getByText(/create react app/i);
     expect(linkElement).toBeInTheDocument(); // Use the toBeInTheDocument matcher
   });
-})
+});
