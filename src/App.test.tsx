bbb2385028f833b9a1520e076be20645a085b5a4
Leandro Ferreira <leanfj@hotmail.com>
2024-04-02 07:22:03 -0300

    chore: iniciando projeto

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import './matchMediaMock';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { act } from 'react-dom/test-utils';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// Extend the type definition of JestMatchers<HTMLElement> to include toBeInTheDocument
+declare global {
+  namespace jest {
+    interface Matchers<R, T> {
+      toBeInTheDocument(): R;
+    }
+  }
+}
+
+describe("App", () => {
+  test('renders learn react link', async () => {
+    // eslint-disable-next-line testing-library/no-unnecessary-act
+    await act( async () => { render(<App/>) });
+    const linkElement = screen.getByText(/create react app/i);
+    expect(linkElement).toBeInTheDocument(); // Use the toBeInTheDocument matcher
+  });
+})
---
+++ src/app-info.tsx
@@ -0,0 +1,5 @@
+const appInfo = {
+    title: 'Barber Shop Web'
+};
+export default appInfo;
+
